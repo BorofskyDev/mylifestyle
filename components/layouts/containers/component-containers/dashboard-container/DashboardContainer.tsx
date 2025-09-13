2f18ec2a5f7667b14f5f56e0c908d0921d6bd2d0
BorofskyDev <joelborofsky@mac.com>
2025-09-12 22:01:04 -0500

    Enhance DashboardContainer layout and styles; add icon support and update spacing variables

--- components/layouts/containers/component-containers/dashboard-container/DashboardContainer.tsx
+++ components/layouts/containers/component-containers/dashboard-container/DashboardContainer.tsx
@@ -1,14 +1,28 @@
-import { Heading } from "@/components/ui";
-import { InternalLink } from "@/components/ui/common/links/internal-link/InternalLink";
-import { GenericPage } from "@/components/layouts";
+import { Heading, Icon, icons } from '@/components/ui'
+import { InternalLink } from '@/components/ui/common/links/internal-link/InternalLink'
+import { GenericPage } from '@/components/layouts'
+import styles from './DashboardContainer.module.scss'
 
 export function DashboardContainer() {
-    return (
-      <GenericPage>
-        <Heading element='h1' variant='page'>
-          Welcome to My Lifestyle
-        </Heading>
-        <InternalLink href='/debt-tracker'>Go to Debt Tracker</InternalLink>
-      </GenericPage>
-    )
+  return (
+    <GenericPage className={styles.dashboardContainer}>
+      <Heading element='h1' variant='page'>
+        Welcome to My Lifestyle
+      </Heading>
+      <section id='main' className={styles.mainSection}>
+        <div className={styles.appContainer}>
+          <Icon path={icons.debt} className='icon-large stroke-emerald' />
+          <InternalLink href='/debt-tracker' className='text-emerald'>Go to Debt Tracker</InternalLink>
+        </div>
+        <div className={styles.appContainer}>
+          <Icon path={icons.budget} className='icon-large stroke-sky' />
+          <InternalLink href='/budget-tracker' className='text-sky'>Go to Budget Tool</InternalLink>
+        </div>
+        <div className={styles.appContainer}>
+          <Icon path={icons.jobSearch} className='icon-large stroke-orange' />
+          <InternalLink href='/job-tracker' className='text-orange'>Go to Job Tracker</InternalLink>
+        </div>
+      </section>
+    </GenericPage>
+  )
 }
