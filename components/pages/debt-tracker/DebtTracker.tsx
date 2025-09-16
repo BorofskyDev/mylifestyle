f4eceb87c0959b5bdfdd367935b65396df1772bf
BorofskyDev <joelborofsky@mac.com>
2025-09-15 21:36:09 -0500

    Enhance styling utilities and mixins
    - Added a new flex-row mixin to facilitate row-based flex layouts.
    - Updated icon sizes for medium and small icons, with responsive adjustments for small icons across breakpoints.
    - Forwarded the new flex mixin in the index file for better accessibility.
    - Enhanced stroke color utilities by adding fill properties for existing colors and introduced a new ruby-2 stroke color.
    - Introduced a new section gap variable for consistent spacing.
    - Created a new utility file for flex styles, applying the flex-col mixin with a section gap.

--- components/pages/debt-tracker/DebtTracker.tsx
+++ components/pages/debt-tracker/DebtTracker.tsx
@@ -4,9 +4,9 @@ import { GenericPage } from '@/components/layouts'
 
 export function DebtTracker() {
   return (
-    <GenericPage>
+    <GenericPage className='page-flex'>
       <div className={styles.debtTracker}>
-        <Icon path={icons.debt} className='icon-large stroke-orange' />
+        <Icon path={icons.debt} className='icon-large stroke-ruby' />
         <Heading element='h1' variant='page'>
           Debt Tracker
         </Heading>
@@ -17,17 +17,19 @@ export function DebtTracker() {
           variant='primary'
           type='button'
           ariaLabel='add debt'
-          className='btn-bg-ruby border-ruby text-ruby-2'
+          className={`btn-bg-ruby border-ruby text-ruby-2 ${styles.btn}`}
         >
-          Add Debt
+          Add Debt{' '}
+          <Icon path={icons.moneyBag} className='icon-small stroke-ruby-2' />
         </Button>
         <Button
           variant='primary'
           type='button'
           ariaLabel='make payment'
-          className='btn-bg-emerald border-emerald text-emerald-2'
+          className={`btn-bg-emerald border-emerald text-emerald-2 ${styles.btn}`}
         >
           Make Payment
+          <Icon path={icons.payment} className='icon-small stroke-emerald-2' />
         </Button>
       </section>
     </GenericPage>
